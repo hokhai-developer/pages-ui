af657aac86bd95b6887aed6d0873f645dabbcb2c
hokhai846 <hokhai846@gmail.com>
2022-07-16 23:28:35 +0700

    basic button image them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from '~/layouts/MainLayout';
 function App() {
     return (
-        <div className="App">
-            <h1>hello word</h1>
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<MainLayout />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
